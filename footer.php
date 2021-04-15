e4e1959abde5244470c37cbda27817a27b02993b
Jia-09 <79925230+Jia-09@users.noreply.github.com>
2021-04-15 17:56:33 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
          <div class="container"><!-- div to hold the content in the middle of the page-->
          <div class="row"> <!-- a row that gives us access to the BS columns-->
          <div class="col-md-6 text-center footer-content">
-             <p><?php bloginfo('name'); ?></p>
+             <p><?php bloginfo('name'); ?></p> 
              <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr” >
                 <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt=“rss” >
             </div>
