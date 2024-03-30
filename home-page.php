ed792bbc98e3c270424789b69b78217d04cec07d
Jia-09 <79925230+Jia-09@users.noreply.github.com>
2024-03-31 04:00:07 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -21,7 +21,7 @@
             <div class="row">
 
 <div class="col-md-4">
-<img class="img-fluid" src="http://170.187.231.66/~mesh21/ wp-content/themes/starter_for_josh_site/images/game.jpg" alt="woman">
+<img class="img-fluid" src="http://170.187.231.66/~mesh21/ wp-content/themes/starter_for_josh_site/images/port.jpg" alt="woman">
 <h4 class="article-title"> A PORTRAIT</h4>
 <P class="date"> Friday November 2030 </P>
 <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
