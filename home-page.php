098148a77a9cdf68f3f395fcfc1e5f237c205f9e
Jia-09 <79925230+Jia-09@users.noreply.github.com>
2024-05-06 04:49:40 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -6,6 +6,9 @@
 <?php get_header();?>   <!--  Tells WordPress to include header.php -->
     <section class="container-fluid aboutbg text-center">
         <div class="container">
+        <lottie-player id="twelfthLottie" src="http://170.187.231.66/~mesh21/sub/wp-content/themes/starter_for_josh_site/images/rocketman.json" background="transparent" speed="1" style="margin:auto width: 200px; height: 200px" direction="1"></lottie-player>
+
+
         <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
         <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
 <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
